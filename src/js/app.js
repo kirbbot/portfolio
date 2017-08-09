498895033530faf5899f70681102c5e4d7d36867
Michelle Kirby <michellekirby@Michelles-MacBook-Pro.local>
2017-08-09 12:48:34 +0200

    Loading json file data

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,14 @@ var app = angular.module('mshhllApp',[
 	'ngAnimate',
 	'ngRoute'
 ]);
+
+app.config(['$routeProvider', function($routeProvider){
+	
+	$routeProvider
+		.when('/project', {
+			templateUrl: 'views/project.htm'
+		})
+		.otherwise({
+			redirectTo: '/index.html'
+		});
+}]);
--- src/js/controllers/maincontroller.js
+++ src/js/controllers/maincontroller.js
@@ -1,3 +1,18 @@
-app.controller('mainController', function(){
+app.controller('mainController', function($scope, $http, $location, $rootScope, $routeParams){
 
+	activate();
+	function activate(){
+		$scope.search = $routeParams;
+
+		$http.get("json/data.json").then(function(data){
+			$scope.projects = data.data;
+			if ($routeParams.Id){
+				angular.forEach($scope.projects, function(item){
+					if(item.id == $routeParams.Id){
+						$scope.project = item; 
+					}
+				})
+			}
+		});
+	};
 });
